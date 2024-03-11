7270b0f31e39e9f31ff05eb2bf957a903fb91430
thejuthunga <thejasthejashree@gmail.com>
2024-03-11 17:19:38 +0530

    local commi

--- src/main/java/com/ff/mailsimulationapp/controller/UserController.java
+++ src/main/java/com/ff/mailsimulationapp/controller/UserController.java
@@ -1,5 +1,22 @@
 package com.ff.mailsimulationapp.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ff.mailsimulationapp.dto.ResponseStructure;
+import com.ff.mailsimulationapp.service.UserService;
+
+@RestController
+@RequestMapping("/app")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+//	@PostMapping
+//	public ResponseEntity<ResponseStructure<String>> sendMail(){
+//		
+//	}
 }
--- src/main/java/com/ff/mailsimulationapp/dto/ResponseStructure.java
+++ src/main/java/com/ff/mailsimulationapp/dto/ResponseStructure.java
@@ -1,5 +1,5 @@
 package com.ff.mailsimulationapp.dto;
 
 public class ResponseStructure<T> {
-
+	
 }
