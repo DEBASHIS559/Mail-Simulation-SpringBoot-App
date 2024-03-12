605eae3b9c13fe9409879283a4fa948872d8ff48
thejuthunga <thejasthejashree@gmail.com>
2024-03-12 10:53:12 +0530

    mail entity and controller done

--- src/main/java/com/ff/mailsimulationapp/controller/UserController.java
+++ src/main/java/com/ff/mailsimulationapp/controller/UserController.java
@@ -1,23 +1,8 @@
 package com.ff.mailsimulationapp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ff.mailsimulationapp.dto.ResponseStructure;
-import com.ff.mailsimulationapp.service.UserService;
-
 @RestController
-@RequestMapping("/app")
 public class UserController {
-	@Autowired
-	private UserService userService;
 	
-	
-	@PostMapping
-	public ResponseEntity<ResponseStructure<String>> sendMail(){
-		
-	}
 }
--- src/main/java/com/ff/mailsimulationapp/repository/MailRepository.java
+++ src/main/java/com/ff/mailsimulationapp/repository/MailRepository.java
@@ -1,9 +1,17 @@
 package com.ff.mailsimulationapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ff.mailsimulationapp.entity.Mail;
 
 public interface MailRepository extends JpaRepository<Mail, Integer>{
-
+	
+//	public List<Mail> findByToUserId(int userid);
+	
+	
+	@Query("SELECT m FROM Mail m JOIN m.toUsers u WHERE u.id = :userId")
+	public List<Mail> findAllByUserInToUsersId(int userId);
 }
