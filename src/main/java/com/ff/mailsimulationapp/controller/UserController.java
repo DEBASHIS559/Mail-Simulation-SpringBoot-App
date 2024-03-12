37ca20622228217985136a9d26c48a5b99964156
DEBASHIS559 <debashisganguly321@gmail.com>
2024-03-12 11:24:46 +0530

    pulling code from dev

--- src/main/java/com/ff/mailsimulationapp/controller/UserController.java
+++ src/main/java/com/ff/mailsimulationapp/controller/UserController.java
@@ -1,5 +1,36 @@
 package com.ff.mailsimulationapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ff.mailsimulationapp.dto.ResponseStructure;
+import com.ff.mailsimulationapp.entity.User;
+import com.ff.mailsimulationapp.service.UserService;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
 public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/register")
+	public ResponseEntity<ResponseStructure<User>> saveUser(@Valid @RequestBody User user) {
+		
+		return userService.createUser(user);
+	}
+	
+	@PostMapping("/login")
+	ResponseEntity<ResponseStructure<User>> login(@Valid @RequestParam String email, @Valid @RequestParam String password) {
+		return userService.login(email, password);
+	}
+	
 
 }
--- src/main/java/com/ff/mailsimulationapp/dao/UserDao.java
+++ src/main/java/com/ff/mailsimulationapp/dao/UserDao.java
@@ -1,8 +1,34 @@
 package com.ff.mailsimulationapp.dao;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+
+import com.ff.mailsimulationapp.entity.User;
+import com.ff.mailsimulationapp.repository.UserRepository;
 
 @Repository
 public class UserDao {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User createUser(User user) {
+		
+		return userRepository.save(user);
+	}
+	
+	public User findUser(String email, String password) {
+		return userRepository.findByEmailAndPassword(email, password);
+	}
+	
+	public User findUserById(int id) {
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent()) {
+			return user.get();
+		}
+		return null;
+	}
 }
